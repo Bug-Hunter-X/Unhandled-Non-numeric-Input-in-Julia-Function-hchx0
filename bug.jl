d71903936075b9ecfbe6f1c4fba5de88b3d795a1
BugHunter-X <askzenai@gmail.com>
2025-01-25 20:54:06 +0100

    Add bug.jl file

---
+++ bug.jl
@@ -0,0 +1,13 @@
+```julia
+function my_function(x)
+  if x > 0
+    return x^2
+  else
+    return -x^2
+  end
+  #This function will produce an error if the input is not a number
+  #It should ideally include error handling such as checking the input type or using try-catch block
+
+
+end
+```
